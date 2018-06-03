a0da3c6a6b554a1f49ff78e761bb45c91593880e
raphael <raph.koh@gmail.com>
2018-06-03 14:16:59 -0700

    Styled popup.html

--- client/popup.js
+++ client/popup.js
@@ -16,9 +16,39 @@ function toggleChat() {
 document.addEventListener('DOMContentLoaded', function() {
   joinSession();
 
-  const checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    toggleChat();
-  }, false);
+  // const checkPageButton = document.getElementById('checkPage');
+  // checkPageButton.addEventListener('click', function() {
+  //   toggleChat();
+  // }, false);
+
+  const createRoomBtn = document.getElementById('create');
+  const joinRoomBtn = document.getElementById('join');
+  const createRoomSubmitBtn = document.getElementById('submit-create');
+  const joinRoomSubmitBtn = document.getElementById('submit-join');
+  createRoomBtn.addEventListener('click', function () {
+    $(".form-container").show(100);
+    $("#form-create").show();
+    $("#form-join").hide();
+  });
+  joinRoomBtn.addEventListener('click', function () {
+    $(".form-container").show(100);
+    $("#form-create").hide();
+    $("#form-join").show();
+  });
+  createRoomSubmitBtn.addEventListener('click', function() {
+    const username = $("#username").val();
+    if (!username) return;
+    const iconTheme = $("#icons").val();
+    console.log(username);
+    console.log(iconTheme);
+  });
+  joinRoomSubmitBtn.addEventListener('click', function() {
+    const username = $("#username").val();
+    if (!username) return;
+    const room = $("#room").val();
+    if (!room) return;
+    console.log(username);
+    console.log(room);
+  });
 
 }, false);
